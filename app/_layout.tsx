497e8852d6ccd235ec53b9d2365ec7ecab7e27a8
blackie360 <felixkent360@gmail.com>
2025-01-05 13:01:26 +0300

    setup fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
 import { SplashScreen, Stack } from "expo-router";
-import "./globals.css";
+import "./global.css";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
-    "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf"),
-    "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
-    "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
-    "Rubik-Extrabold": require("./assets/fonts/Rubik-Extrabold.ttf"),
-    "Rubik-SemiBold": require("./assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
 
   useEffect(() => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+
+      },
+      colors:{
+        "primary": {
+          "100": "#EDF2F7", 
+          "200": "#A0AEC0",
+          "300": "#2A4365",
+          "400": "#1E3A8A"
+        },
+        "accent": {
+          "100": "#63B3ED",
+          "200": "#F6E05E"
+        },
+        "neutral": {
+          "DEFAULT": "#FFFFFF",
+          "100": "#E2E8F0",
+          "200": "#2D3748"
+        },
+        "danger": "#E53E3E",
+        "success": "#2F855A"
+      }
+    },
   },
   plugins: [],
 }
